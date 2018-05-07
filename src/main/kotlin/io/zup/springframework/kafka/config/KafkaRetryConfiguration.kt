47c2c50c3b291fe62bb99414e9a66f45f1577237
Diego Silveira <diegossilveira@gmail.com>
2018-05-07 18:27:26 -0300

    Breaking config apart

---
+++ src/main/kotlin/io/zup/springframework/kafka/config/KafkaRetryConfiguration.kt
@@ -0,0 +1,20 @@
+package io.zup.springframework.kafka.config
+
+import io.zup.springframework.kafka.annotation.RetryAwareKafkaListenerAnnotationBeanPostProcessor
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.config.KafkaListenerConfigUtils
+import org.springframework.kafka.config.KafkaListenerEndpointRegistry
+
+@Configuration
+class KafkaRetryConfiguration {
+
+    @Bean(name = [KafkaListenerConfigUtils.KAFKA_LISTENER_ENDPOINT_REGISTRY_BEAN_NAME])
+    open fun defaultKafkaListenerEndpointRegistry(): KafkaListenerEndpointRegistry =
+        KafkaListenerEndpointRegistry()
+
+    @Bean(KafkaListenerConfigUtils.KAFKA_LISTENER_ANNOTATION_PROCESSOR_BEAN_NAME)
+    open fun retryAwareKafkaListenerAnnotationBeanPostProcessor(): RetryAwareKafkaListenerAnnotationBeanPostProcessor<*, *> =
+        RetryAwareKafkaListenerAnnotationBeanPostProcessor<Any, Any>()
+
+}
