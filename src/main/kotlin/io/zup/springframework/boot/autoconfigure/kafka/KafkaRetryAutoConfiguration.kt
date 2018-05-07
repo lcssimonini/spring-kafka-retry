1a5ab69abae5eadc42796d471b1bdf8e9fea3897
Diego Silveira <diegossilveira@gmail.com>
2018-05-07 18:44:54 -0300

    Autoconfig is not a good idea

--- src/main/kotlin/io/zup/springframework/boot/autoconfigure/kafka/KafkaRetryAutoConfiguration.kt
+++
@@ -1,9 +0,0 @@
-package io.zup.springframework.boot.autoconfigure.kafka
-
-import io.zup.springframework.kafka.config.KafkaRetryConfiguration
-import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Import
-
-@Configuration
-@Import(KafkaRetryConfiguration::class)
-open class KafkaRetryAutoConfiguration
